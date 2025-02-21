31fcec5be0c3e37d5e6ff01f457075044bb44783
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-02-21 22:02:21 +0530

    Rename column name from task to title

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_20_104849) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_21_161457) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.text "task"
+    t.text "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
