69747ec707584e84d516bfbac74418de64cde3f0
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-02-24 15:43:20 +0530

    add deadline and status column in tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_21_161457) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_23_171350) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,5 +18,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_21_161457) do
     t.text "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
+    t.datetime "deadline"
   end
 end
